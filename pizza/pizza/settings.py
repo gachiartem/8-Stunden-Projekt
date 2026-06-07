f83fd1d696f0219dc717627a95b03facaf410082
artemkagrif2007 <gachiartem@gmail.com>
2026-06-08 00:43:02 +0200

    сделали views/urls/admin

--- pizza/pizza/settings.py
+++ pizza/pizza/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY= 'django-insecure-^ha=646bw1=_n&%u#6z&8(qb$yx$@-on&)^lxj7a&4m&o369-e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
